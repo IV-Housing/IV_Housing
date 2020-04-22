ea1a89f0dd3b913a91a14208f4f3af0a676b7ae2
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-04-21 17:49:23 -0700

    mk - did npm init/install commands, deleted seth's index.jsx, and commented everything out of index.js and wrote new index function

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
-import Layout from "../components/Layout";
-import { optionalAuth } from "../utils/ssr";
+// import Layout from "../components/Layout";
+// import { optionalAuth } from "../utils/ssr";
+//
+// export const getServerSideProps = optionalAuth;
 
-export const getServerSideProps = optionalAuth;
+// function HomePage(props) {
+// const user = props.user;
+//
+// return (
+// <Layout user={user}>
+// {user ? (
+// <div>
+// You're logged in! Here's what the server knows about you:
+// <pre>{JSON.stringify(user, null, "\t")}</pre>
+// </div>
+// ) : (
+// <div>You're not logged in!</div>
+// )}
+// </Layout>
+// );
+// }
+//
+// export default HomePage;
 
-function HomePage(props) {
-  const user = props.user;
-
-  return (
-    <Layout user={user}>
-      {user ? (
-        <div>
-          You're logged in! Here's what the server knows about you:
-          <pre>{JSON.stringify(user, null, "\t")}</pre>
-        </div>
-      ) : (
-        <div>You're not logged in!</div>
-      )}
-    </Layout>
-  );
+export default function index() {
+  return <p>Application coming soon</p>;
 }
-
-export default HomePage;
