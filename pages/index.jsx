0b01773b934e6ad0272a832a36eadd5bc4a922fb
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-04-28 15:52:47 -0700

    fixed commented code, commented filename, indentation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,14 @@
-// index.js
+// index.jsx
+
 import React from 'react'
-//import '../css/index.css'
 import HouseTable from '../components/table.jsx'
 import data from '../data/houses.json'
 
 export default function Index() {
-	return(
+	return (
 		<div className="page-container">
 			<h1>House List</h1>
 			<HouseTable data = {data}/>
 		</div>
-		)
+	)
 }
