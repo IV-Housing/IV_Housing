4b7460b0c6e48f4efd6a10df59b4cafdf6da4ac2
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-05-01 18:06:55 -0700

    mk - factored out filterForm from index.jsx to a new component FilterForm in form.jsx. Also cleaned up some indentation in index.jsx and factored out a styling div from index.jsx into the HouseTable component to make index.jsx cleaner

---
+++ components/form.jsx
@@ -0,0 +1,58 @@
+// form.jsx
+
+import utilStyles from '../styles/utils.module.css'
+
+export default function FilterForm() {
+	return (
+		<div className={utilStyles.filterForm}>
+			<h2>Filter Results</h2>
+			<p>Here you can refine the details to find the perfect listing!</p>
+
+			<div className={utilStyles.filterFormSelects}>
+				<label>Choose a Street:</label>
+				<select id="street-values">
+					<option value = "default" selected> Any </option>
+					<option value = "cervantes">Cervantes Rd.</option>
+					<option value = "elgreco">El Greco Rd.</option>
+					<option value = "picasso">Picasso Rd.</option>
+					<option value = "segovia">Segovia Rd.</option>
+					<option value = "cordoba">Cordoba Rd.</option>
+					<option value = "pardall">Pardall Rd.</option>
+					<option value = "madrid">Madrid Rd.</option>
+					<option value = "seville">Seville Rd.</option>
+					<option value = "trigo">Trigo Rd.</option>
+					<option value = "elnido">El Nido Ln.</option>
+					<option value = "sabadotarde">Sabado Tarde Rd.</option>
+					<option value = "delplaya">Del Playa Dr.</option>
+					<option value = "elcolegio">El Cologio Rd.</option>
+					<option value = "abrego">Abrego Rd.</option>
+					<option value = "sueno">Sueno Rd.</option>
+					<option value = "pasado">Pasado Rd.</option>
+					<option value = "estero">Estero Rd.</option>
+					<option value = "fortuna">Fortuna Rd.</option>
+					<option value = "embarcaderodelnorte">Embarcadero Del Norte</option>
+					<option value = "embarcaderodelmar">Embarcadero Del Mar</option>
+					<option value = "caminodelpescadero">Camino Del Pescadero</option>
+					<option value = "caminodelsur">Camino Del Sur</option>
+					<option value = "caminocorto">Camino Corto</option>
+					<option value = "caminolindo">Camino Lindo</option>
+				</select>
+				
+				<label>Choose a Block:</label>
+				<select id="block-values">
+					<option value = "default" selected>Any</option>
+					<option value = "five"> 65 Block </option>
+					<option value = "six">  66 Block </option>
+					<option value = "seven"> 67 Block </option>
+					<option value = "eight"> 68 Block </option>
+				</select>
+
+				<label>Sort Price From:</label>
+				<select id="price-values">
+					<option value = "l-to-h" selected> Lowest to Highest</option>
+					<option value = "h-to-l">  Highest to Lowest </option>
+				</select>
+			</div>
+		</div>
+	);
+}
