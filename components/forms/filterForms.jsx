517ee70286cfe554a528ad1766a99f0902cd9139
Congyuan Sun <congyuan@ucsb.edu>
2020-05-06 06:28:24 +0800

    .

--- components/forms/filterForms.jsx
+++ components/forms/filterForms.jsx
@@ -1,6 +1,8 @@
 // filterForms.jsx
 
 import utilStyles from '../../styles/utils.module.css'
+import data from '../../data/houses.json'
+import HouseTable from '../table.jsx'
 
 export default function FilterForms() {
 	return (
@@ -45,3 +47,41 @@ export default function FilterForms() {
 		</div>
 	);
 }
+
+
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "default") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
