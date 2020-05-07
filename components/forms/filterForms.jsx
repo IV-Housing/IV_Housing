11af7e6a2c81c3f2a1ccfa0ac0d55d32514aff9f
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-05-06 21:30:30 -0700

    mk - removed commented out code

--- components/forms/filterForms.jsx
+++ components/forms/filterForms.jsx
@@ -64,51 +64,6 @@ class FilterForms extends React.Component {
 }
 export default FilterForms
 
-// export default function FilterForms(props) {
-// 	return (
-// 		<div className={utilStyles.formSelects}>
-// 			<label>Choose a Street:</label>
-// 			<select id="street-values" defaultValue="default" onChange={props.filter}>
-// 				<option value = "default"> Any </option>
-// 				<option value = "cervantes">Cervantes Rd.</option>
-// 				<option value = "elgreco">El Greco Rd.</option>
-// 				<option value = "picasso">Picasso Rd.</option>
-// 				<option value = "segovia">Segovia Rd.</option>
-// 				<option value = "cordoba">Cordoba Rd.</option>
-// 				<option value = "pardall">Pardall Rd.</option>
-// 				<option value = "madrid">Madrid Rd.</option>
-// 				<option value = "seville">Seville Rd.</option>
-// 				<option value = "trigo">Trigo Rd.</option>
-// 				<option value = "elnido">El Nido Ln.</option>
-// 				<option value = "sabadotarde">Sabado Tarde Rd.</option>
-// 				<option value = "delplaya">Del Playa Dr.</option>
-// 				<option value = "elcolegio">El Cologio Rd.</option>
-// 				<option value = "abrego">Abrego Rd.</option>
-// 				<option value = "sueno">Sueno Rd.</option>
-// 				<option value = "pasado">Pasado Rd.</option>
-// 				<option value = "estero">Estero Rd.</option>
-// 				<option value = "fortuna">Fortuna Rd.</option>
-// 				<option value = "embarcaderodelnorte">Embarcadero Del Norte</option>
-// 				<option value = "embarcaderodelmar">Embarcadero Del Mar</option>
-// 				<option value = "caminodelpescadero">Camino Del Pescadero</option>
-// 				<option value = "caminodelsur">Camino Del Sur</option>
-// 				<option value = "caminocorto">Camino Corto</option>
-// 				<option value = "caminolindo">Camino Lindo</option>
-// 			</select>
-
-// 			<label>Choose a Block:</label>
-// 			<select id="block-values" defaultValue="default">
-// 				<option value = "default">Any</option>
-// 				<option value = "five"> 65 Block </option>
-// 				<option value = "six">  66 Block </option>
-// 				<option value = "seven"> 67 Block </option>
-// 				<option value = "eight"> 68 Block </option>
-// 			</select>
-// 		</div>
-// 	);
-// }
-
-
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
