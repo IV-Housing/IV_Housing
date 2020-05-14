86be0d899bf95e5e03300ebacd646b02de96a8d8
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2020-05-14 16:45:36 -0700

    sv -added divs for comaprison tiles

--- components/mapComp.jsx
+++ components/mapComp.jsx
@@ -7,26 +7,36 @@ export default function MapView(props){
     
     return(
         <div>
-            <ReactMapGL 
-                {...viewport} 
-                mapboxApiAccessToken={"pk.eyJ1Ijoic2V0aHZhbmIiLCJhIjoiY2thNjhtOGh4MDVtODJzbW5jbHV1cmc3aiJ9.uXYwbhfNVaCx8_plHRewUg"}
-                mapStyle="mapbox://styles/sethvanb/cka744x8c16b31ilhulkr0d26"
-                onViewportChange = {viewport => {setViewport(viewport);}}
-             >
-            {/*<div> 
-                var house_coordinates;
-                geocoding.forwardGeocode({
-                    query: "6666 Del Playa Dr.",
-                    latitude: house_coordinates.lat,
-                    longitude: house_coordinates.lng
-                })
+            <div style = "display: inline-flex; flex-direction: column">
+                <div style = "border: 1px solid black; height: 300px; width: 300px">
+                    <p>compare 1</p>
+                </div>
+                <div style = "border: 1px solid black; height: 300px; width: 300px">
+                    <p>compare 2</p>
+                </div>
             </div>
-             {props.list.address.map(house => (
-                <Marker key={house} latitude={parkgeometry}>
-                    <div>HOUSE</div>
-                </Marker>
-            ))} */}
-            </ReactMapGL>
+            <div>
+                <ReactMapGL 
+                    {...viewport} 
+                    mapboxApiAccessToken={"pk.eyJ1Ijoic2V0aHZhbmIiLCJhIjoiY2thNjhtOGh4MDVtODJzbW5jbHV1cmc3aiJ9.uXYwbhfNVaCx8_plHRewUg"}
+                    mapStyle="mapbox://styles/sethvanb/cka744x8c16b31ilhulkr0d26"
+                    onViewportChange = {viewport => {setViewport(viewport);}}
+                >
+                {/*<div> 
+                    var house_coordinates;
+                    geocoding.forwardGeocode({
+                        query: "6666 Del Playa Dr.",
+                        latitude: house_coordinates.lat,
+                        longitude: house_coordinates.lng
+                    })
+                </div> 
+                {props.list.address.map(house => (
+                    <Marker key={house} latitude={parkgeometry}>
+                        <div>HOUSE</div>
+                    </Marker>
+                ))}*/}
+                </ReactMapGL>
+            </div>
         </div>
     )
 }
