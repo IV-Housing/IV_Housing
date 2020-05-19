62e10d6839a9492154e8e7198b4f8681108502ce
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-05-19 16:41:02 -0700

    mk - fixed small issue with map page filters

--- pages/map.jsx
+++ pages/map.jsx
@@ -11,7 +11,7 @@ import Info from '../components/info.jsx'
 export default function Map(){
 	const [initList, setInitList] = useState([]);  // houses data
 	const [refinedData, setRefinedData] = useState([]);
-	const [{street, block,  size, price, priceType}, setStreetBlock] = useState({street:'Any', block:'Any', size:'Any', price:'Any', priceType:'total'});
+	const [{street, block,  size, price, priceType}, setStreetBlock] = useState({street:'Any', block:'Any', size:'Any', price:'Any Price', priceType:'total'});
 	
 	useEffect(() => { if(initList.length===0) getList();});
 
@@ -30,50 +30,51 @@ export default function Map(){
 		let houses = [...initList];
 		let s = document.getElementById('streetSelect');
 		let b = document.getElementById('blockSelect');
+		let p = document.getElementById('priceSelect');
+		let si = document.getElementById('sizeSelect');
 
-		if(street!=='Any') {
-			houses = houses.filter(item=>{
-				return item.address.indexOf(street)!==-1
-			 })
-		 }
-		 if(block!=='Any') {
-			 houses = houses.filter(item=>{
-				return item.address.substr(0,2) === block
-			})
-		 }
-		 if(size!=='Any') {
+		if (street!=='Any') { 
+			houses = houses.filter((item)=>{ return item.address.indexOf(street)!==-1; }) 
+			if (!s.classList.contains(utilStyles['active'])) s.classList.toggle(utilStyles['active']);	
+		} else { if (s.classList.contains(utilStyles['active'])) s.classList.toggle(utilStyles['active']); }
+
+		if (block!=='Any') { 
+			houses = houses.filter((item)=>{ return item.address.substr(0,2) === block}) 
+			if (!b.classList.contains(utilStyles['active'])) b.classList.toggle(utilStyles['active']);	
+		} else { if (b.classList.contains(utilStyles['active'])) b.classList.toggle(utilStyles['active']); }
+
+		if(size!=='Any') {
 			houses = houses.filter(item=>{
 				let tIndex=size.indexOf('+')
-				if(tIndex!==-1){
-					return item.size > Number(size.substring(0,tIndex))
-				}else{
-					return item.size === Number(size)
-				}
-		   })
-		}
-		if(price!=='Any') {
-			 if(priceType=='total'){
+				if (tIndex!==-1) return item.size > Number(size.substring(0,tIndex))
+				else return item.size === Number(size)
+			})
+			if (!si.classList.contains(utilStyles['active'])) si.classList.toggle(utilStyles['active']);	
+		} else { if (si.classList.contains(utilStyles['active'])) si.classList.toggle(utilStyles['active']); }
+
+		if (price!=='Any Price') {
+			 if (priceType==='total'){
 				houses = houses.filter(item=>{
 					let tIndex=price.indexOf('+')
-					if(tIndex!==-1){
-						return item.totalPrice >= Number(price.substring(0,tIndex))
-					}else{
+					if (tIndex!==-1) return item.totalPrice >= Number(price.substring(0,tIndex))
+					else {
 						let tarr=price.split('-')
 						return item.totalPrice >= Number(tarr[0])&&item.totalPrice < Number(tarr[1])
 					}
 				})
-			 }else{
+			 } else {
 				houses = houses.filter(item=>{
 					let tIndex=price.indexOf('+')
-					if(tIndex!==-1){
+					if (tIndex!==-1) {
 						return Number((item.totalPrice/item.size).toFixed(2)) >= Number(price.substring(0,tIndex))
-					}else{
+					} else {
 						let tarr=price.split('-')
 						return Number((item.totalPrice/item.size).toFixed(2)) >= Number(tarr[0])&&Number((item.totalPrice/item.size).toFixed(2)) < Number(tarr[1])
 					}
 				})
 			 }
-		}
+			if (!p.classList.contains(utilStyles['active'])) p.classList.toggle(utilStyles['active']);	
+		} else { if (p.classList.contains(utilStyles['active'])) p.classList.toggle(utilStyles['active']); }
 
 		setRefinedData(houses);
 	}, [initList, street, block,  size, price, priceType]);
