3d08ad2a9ac98d7ae0a08b8717c6487be639ebd3
Moshe Haiem <mhaiem9@gmail.com>
2020-05-20 17:00:13 -0700

    mh finished basics of lab06. need to work on each of the storybooks

---
+++ stories/dataView.stories.js
@@ -0,0 +1,12 @@
+import DataView from "../components/view/dataView";
+
+import React from "react";
+
+export default {
+  title: "DataView",
+  component: DataView,
+};
+
+export const simple_dataView = () => {
+  return <DataView />;
+};
