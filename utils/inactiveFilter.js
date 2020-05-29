345a26ef3f73ee5ffdc0801aff17307906fca2c1
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-05-29 00:48:08 -0700

    mk - factored out all sort/filter code in index.jsx into functions in the utils folder. Also DRYed up code by factoring out similar code in index.jsx/map.jsx into a function.

--- utils/inactiveFilter.js
+++
@@ -1,7 +0,0 @@
-// inactiveFilter.js
-
-import utilStyles from '../styles/utils.module.css'
-
-export function markInactiveFilter(element) {
-	if (element.classList.contains(utilStyles['active'])) element.classList.toggle(utilStyles['active']);
-}
