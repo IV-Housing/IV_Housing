7e545d17964def93d313111a5d77f43e5bb1c7a6
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-06-05 11:19:58 -0700

    mk - fixed FontAwesome laggy css bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
+// _app.js
+
 import '../styles/global.css'
+// fixes fontawesome css lag:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export default function App({Component,pageProps}){
   return <Component {...pageProps} />
